0fcff75584cf4938e497c9ef2c09f64841b1238a
Youssef Shaaban <ys1300@fayoum.edu.eg>
2023-03-26 20:32:21 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      //   primarySwatch: Colors.blue,
-      home: FirstApp(),
-    );
-  }
-}
-
-class FirstApp extends StatelessWidget {
-  const FirstApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.teal,
         title: const Text("First Application"),
+        backgroundColor: Colors.teal,
       ),
-      body: Center(
-        // ignore: avoid_unnecessary_containers
-        child: Container(
-          child: const Text(
-            "Youssef Shaaban Mohamed",
-            style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
-          ),
+      body: const Center(
+          child: Text(
+        "Youssef Shaaban Mohammed",
+        style: TextStyle(
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
         ),
-      ),
-    );
-  }
+      )),
+    ),
+  ));
 }
